94c5b2f6e939154a30bbd4fe6fd1574b62e3520d
Linus Gates <groggyegg@protonmail.com>
2021-06-23 21:32:19 +0200

    Update database and asianload

--- resources/lib/resolveurl/plugins/asianload.py
+++ resources/lib/resolveurl/plugins/asianload.py
@@ -17,8 +17,6 @@
 """
 
 import re
-from resolveurl.plugins.lib import helpers
-from resolveurl import common
 from resolveurl.resolver import ResolveUrl
 
 
@@ -28,14 +26,14 @@ class AsianLoadResolver(ResolveUrl):
     pattern = r'(?://|\.)(asianload\.io)/\w+\.php\?(.+)'
 
     def get_media_url(self, host, media_id):
-        headers = {'User-Agent': common.RAND_UA, 'X-Requested-With': 'XMLHttpRequest'}
-        html = self.net.http_GET(self.get_url(host, media_id), headers=headers).content
-        sources = [(label, url.replace('\\/', '/')) for (url, label) in re.findall(r'"source(?:_bk)?":\[{"file":"([^"]+)","label":"([^"]+)"', html)]
-        headers.update({'Referer': host})
-        return helpers.pick_source(sources) + helpers.append_headers(headers)
+        html = self.net.http_GET(self.get_url(host, media_id)).content
+        match = re.search(r'<div class="dowload">.+?href="([^"]+)"', html, re.DOTALL)
+
+        if match:
+            return match.group(1)
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/ajax.php?{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/video.php?{media_id}')
 
     @classmethod
     def _is_enabled(cls):
